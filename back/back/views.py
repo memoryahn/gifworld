8f4ce663baf5d2bca0bea3b30f8ad5bbfabe92f5
SSAhn <memoryahn@gmail.com>
2018-01-31 20:01:35 +0900

    엠팍 몽고디비저장 및 index에 출력

--- back/back/views.py
+++ back/back/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from pymongo import MongoClient
+
 # from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'index.html', {})
+def index(request):    
+    client = MongoClient("mongodb://220.230.124.148:27017")
+    db = client.gif
+    gifcoll = db.gifcoll
+    data = gifcoll.find() 
+    client.close()
+    return render(request, 'index.html', {'data':data})
+
