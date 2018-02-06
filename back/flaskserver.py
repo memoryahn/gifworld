c301166a545bd2433bb74d4c2c55d256a62ca95c
labtopSSAhn <memoryahn@gmail.com>
2018-02-06 17:02:12 +0900

    flask CORS 및 리눅스용 호스트포트 주석처리

--- back/flaskserver.py
+++ back/flaskserver.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 app.config['MONGO_DBNAME'] = 'gif'
 app.config['MONGO_HOST'] = 'mongodb://220.230.124.148:27017'
+# app.config['MONGO_HOST'] = 'mongodb://127.0.0.1:27017'
 app.config['MONGO_PORT'] = '27017'
 
 mongo = PyMongo(app)
@@ -30,3 +33,4 @@ def get_gif_count(count):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='0.0.0.0',port='5000',debug=True)
